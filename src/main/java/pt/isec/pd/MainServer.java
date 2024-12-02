db7b949e7b94b8c43e129a77212ec75e9ea39693
joaoalmas84 <joaoalmas84@gmail.com>
2024-12-02 18:43:16 +0000

    MainClient

--- src/main/java/pt/isec/pd/MainServer.java
+++ src/main/java/pt/isec/pd/MainServer.java
@@ -23,7 +23,7 @@ import java.sql.SQLException;
 @SpringBootApplication
 @EnableConfigurationProperties(RsaKeysProperties.class)
 @ConfigurationPropertiesScan
-public class MainServer {
+public class    MainServer {
 
     private RsaKeysProperties rsaKeysProperties;
 
